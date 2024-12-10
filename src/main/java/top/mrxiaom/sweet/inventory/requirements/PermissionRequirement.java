1bc8e2599aa4d88679f7cab4f752da863b6b0a0e
人間工作 <coolxiaom95@gmail.com>
2024-12-10 13:33:48 +0800

    add simple deserializer to perm requirement

--- src/main/java/top/mrxiaom/sweet/inventory/requirements/PermissionRequirement.java
+++ src/main/java/top/mrxiaom/sweet/inventory/requirements/PermissionRequirement.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import top.mrxiaom.pluginbase.utils.PAPI;
 import top.mrxiaom.sweet.inventory.func.menus.MenuInstance;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +13,9 @@ public class PermissionRequirement implements IRequirement {
     final String permission;
     final List<String> denyCommands;
 
+    PermissionRequirement(String permission) {
+        this(permission, new ArrayList<>());
+    }
     PermissionRequirement(String permission, List<String> denyCommands) {
         this.permission = permission;
         this.denyCommands = denyCommands;
@@ -20,6 +24,7 @@ public class PermissionRequirement implements IRequirement {
     protected static void init(RequirementsRegistry registry) {
         registry.deserializers.put("permission", PermissionRequirement::deserializer);
         registry.deserializers.put("权限", PermissionRequirement::deserializer);
+        registry.simpleDeserializers.add(PermissionRequirement::simpleDeserializer);
     }
 
     protected static IRequirement deserializer(boolean alt, ConfigurationSection section, String key) {
@@ -27,6 +32,11 @@ public class PermissionRequirement implements IRequirement {
         if (permission == null) return null;
         List<String> denyCommands = section.getStringList(key + (alt ? ".不满足需求执行" : ".deny-commands"));
         return new PermissionRequirement(permission, denyCommands);
+    }
+    protected static IRequirement simpleDeserializer(String str) {
+        if (str.startsWith("perm ")) return new PermissionRequirement(str.substring(5));
+        if (str.startsWith("权限 ")) return new PermissionRequirement(str.substring(3));
+        return null;
     }
 
     @Override
